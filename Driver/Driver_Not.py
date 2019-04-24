7526f4cf75d8cbac6b4361045a8d8917024f987c
Charlemagne Wong <frian13@hotmail.com>
2019-04-23 22:07:31 -0500

    Stuff

--- Driver/Driver_Not.py
+++ Driver/Driver_Not.py
@@ -3,19 +3,18 @@ from csce420_final_project.NeuralNetwork import Example
 import numpy as np
 from random import randint
 
-xorAI = NeuralNetwork([2, 4, 1], 0.1)
+xorAI = NeuralNetwork([1, 2, 2, 1], 0.1)
 
-e1 = Example(np.array([[0], [0]]), np.array([[1]]))
-e2 = Example(np.array([[0], [1]]), np.array([[1]]))
-e3 = Example(np.array([[1], [0]]), np.array([[0]]))
-e4 = Example(np.array([[1], [1]]), np.array([[1]]))
+e1 = Example(np.array([[0]]), np.array([[1]]))
+e2 = Example(np.array([[1]]), np.array([[0]]))
 
-e = [e1, e2, e3, e4]
+
+e = [e1, e2]
 
 MAX_LEARN = 10000
 example_list = []
 for i in range(MAX_LEARN):
-    r = randint(0, 3)
+    r = randint(0, 1)
     example_list.append(e[r])
     # example_list.append(Example(np.array([[a],[b]]), np.array([[ int(a == 1 or b == 1) ]])))
 
@@ -26,12 +25,11 @@ correct = 0
 
 for i in range(total):
     a = randint(0, 1)
-    b = randint(0, 1)
-    c = xorAI.output((np.array([[a],[b]])))
+    c = xorAI.output((np.array([[a]])))
 
     # if int(a == 1 or b == 1) == int(c[0][0] + 0.5):
     #     correct = correct + 1
 
-    print('Set [{},{}] -> {} | AI: {}'.format(a, b, int(not(a == 1 and b == 0)), c))
+    print('Set [{}] -> {} | AI: {}'.format(a, int(not(a == 1)), c))
 
 print('Correct: {}%'.format(correct / total * 100))
